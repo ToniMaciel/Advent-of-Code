6a685c1dc3c175731a5c5748f3b2d225d3e8ee9d
ToniMaciel <tonialvesmaciel@gmail.com>
2023-12-11 16:49:16 -0300

    day 11

---
+++ day11/CosmicExpansion.java
@@ -0,0 +1,85 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.HashSet;
+
+public class CosmicExpansion {
+    static List<SimpleEntry<Integer, Integer>> galaxies = new ArrayList<>();
+
+    public static void main(String[] args) {
+        String filePath = "input.txt";
+        File file = new File(filePath);
+
+        readInput(file, filePath);
+        calculateDistancesBetweenGalaxies();
+    }
+
+    private static void readInput(File file, String filePath) {
+        
+        try {
+            Scanner scanner = new Scanner(file);
+            Set<Integer> columnsWithGalaxies = new HashSet<>();
+            int rows = 0;
+            int columns = 0;
+
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                columns = line.length();
+                rows++;
+
+                if (!line.contains("#")) {
+                    rows++;
+                } else {
+                    for (int i = 0; i < line.length(); i++) {
+                        if (line.charAt(i) == '#') {
+                            columnsWithGalaxies.add(i);
+                            galaxies.add(new SimpleEntry<>(rows - 1, i));
+                        }
+                    }
+                }
+            }
+            
+            shiftRightGalaxies(columnsWithGalaxies, columns);
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + filePath);
+        }
+
+    }
+
+    private static void shiftRightGalaxies(Set<Integer> columnsWithGalaxies, int columns) {
+        Set<Integer> emptyColumns = IntStream.range(0, columns)
+                .boxed()
+                .collect(Collectors.toSet());
+        emptyColumns.removeAll(columnsWithGalaxies);
+
+        galaxies.forEach(galaxy -> {
+            int column = galaxy.getValue();
+            int shift = (int) emptyColumns.stream().filter(c -> c < column).count();
+            galaxy.setValue(column + shift);
+        });
+    }
+
+    private static void calculateDistancesBetweenGalaxies() {
+        int sumDistances = 0;
+        for (int i = 0; i < galaxies.size() - 1; i++) {
+            SimpleEntry<Integer, Integer> galaxy1 = galaxies.get(i);
+            for (int j = i + 1; j < galaxies.size(); j++) {
+                SimpleEntry<Integer, Integer> galaxy2 = galaxies.get(j);
+                int distance = getDistance(galaxy1, galaxy2);
+                sumDistances += distance;
+            }
+        }
+        System.out.println(sumDistances);
+    }
+
+    private static int getDistance(SimpleEntry<Integer, Integer> galaxy1, SimpleEntry<Integer, Integer> galaxy2) {
+        return Math.abs(galaxy1.getKey() - galaxy2.getKey()) + Math.abs(galaxy1.getValue() - galaxy2.getValue());
+    }
+}
